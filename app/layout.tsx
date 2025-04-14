4babfdec8bbc20e4ac75f56fd74c151829cf5803
Rafael Silva <rafael.silva@tecnorisk.com.br>
2025-04-14 12:55:59 -0300

    refactor: clean up global styles and update layout language to Portuguese

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,9 +23,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+    <html lang="pt-BR">
+      <body 
+        className={`${geistSans.variable} ${geistMono.variable} bg-gray-100 antialiased`}
       >
         {children}
       </body>
