75449718af65f966d72cf98955f857b5763dedf5
Rafael Silva <rafael.silva@tecnorisk.com.br>
2025-04-16 17:33:03 -0300

    UP

--- app/lib/firebase.ts
+++ app/lib/firebase.ts
@@ -1,21 +1,29 @@
-import { initializeApp } from "firebase-admin";
+import * as admin from 'firebase-admin'
 import { cert, getApps } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 import "server-only";
+// const decodedKey = Buffer.from(process.env.FIREBASE_PRIVATE_KEY_BASE64 || "", "base64").toString("utf-8");
+// console.log(`Decoded key: ${decodedKey}`)
 
-const firebaseCert = cert({
+export const firebaseCert = cert({
   projectId: process.env.FIREBASE_PROJECT_ID,
   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n")
 })
-
-if(!getApps().length){
-  initializeApp({
-    credential: firebaseCert,
-    // databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-    // storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  })
-
+try {
+  if(!getApps().length){
+    admin.initializeApp({
+      credential: firebaseCert,
+      // databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+      // storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    })
+  
+  }
+  
+} catch (error) {
+  console.error("Error initializing Firebase Admin SDK:", error);
+  throw error;
+  
 }
 
 export const db = getFirestore()
